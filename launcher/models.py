7a9b76a45fc1d08a58ee92789f735836cc7347a4
Victor Carceler <vcarceler@iespuigcastellar.xeill.net>
2020-12-26 12:19:36 +0100

    Añadido model request

--- launcher/models.py
+++ launcher/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Request(models.Model):
+    address = models.GenericIPAddressField()
+    datetime = models.DateTimeField()
+
+    def __str__(self):
+        return "address: " + self.address + " datetime: " + self.datetime 
--- sirin/settings.py
+++ sirin/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'launcher.apps.LauncherConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
