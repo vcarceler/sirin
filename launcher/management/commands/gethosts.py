d591d9233c1ddf6d0795b99bfb6d0104bf792a3c
Victor Carceler <vcarceler@iespuigcastellar.xeill.net>
2020-12-27 20:00:47 +0100

    python manage.py gethosts

--- launcher/management/commands/gethosts.py
+++ launcher/management/commands/gethosts.py
@@ -2,11 +2,13 @@ from django.core.management.base import BaseCommand, CommandError
 from launcher.models import Request
 
 class Command(BaseCommand):
-    help = 'Get a list of hosts with not processed requests and marks requests as processed.'
+    help = 'Get a list of hosts with not processed requests and then marks requests as processed.'
 
-    def add_arguments(self, parser):
+    #def add_arguments(self, parser):
         #parser.add_argument('get_ansible_limit', nargs='+', type=int)
-        a=1
-
+    
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('Ansible limit: options: "%s"' % options ))
+        for r in Request.objects.filter(processed=False):
+            self.stdout.write('{0},'.format(r.address), ending='')
+            r.processed=True
+            r.save()
