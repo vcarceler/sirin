3fdd7480cb49dbb975b229a87e40530937387bd1
Victor Carceler <vcarceler@iespuigcastellar.xeill.net>
2020-12-29 20:26:56 +0100

    Añadida label a request

--- launcher/models.py
+++ launcher/models.py
@@ -9,9 +9,10 @@ class Request(models.Model):
     address = models.GenericIPAddressField()
     datetime = models.DateTimeField()
     processed = models.BooleanField(default=False)
+    label = models.CharField(max_length=64, default='default')
 
     def __str__(self):
-        return "address: " + self.address + " datetime: " + str(self.datetime) + " processed: " + str(self.processed)
+        return "address: " + self.address + " datetime: " + str(self.datetime) + " processed: " + str(self.processed)  + " label: " + self.label
 
     # Cierto si la solicitud ya ha superado EXCLUSION_PERIOD
     def need_update(self):
